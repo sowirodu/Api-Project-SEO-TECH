65a6500b5b9047d677e6325521f5fabef718a40e
Seth Owirodu <sowirodu@trinity.edu>
2023-06-22 21:44:21 +0000

    Added Database file

--- apiproject.py
+++ apiproject.py
@@ -1,13 +1,24 @@
 import requests
+import sqlalchemy as db
+import pandas as pd
 
 response = requests.get('https://pokeapi.co/api/v2/pokemon/ditto')
 pokemon_data = response.json()
 
-if 'abilities' in pokemon_data:
-    abilities = pokemon_data['abilities']
-    print("Ditto's Abilities:")
-    for ability in abilities:
-        ability_name = ability['ability']['name']
-        print(ability_name)
-else:
-    print("No abilities found for Ditto.")
+# Extract relevant data from pokemon_data dictionary
+pokemon_info = {
+    'Name': [pokemon_data['name']],
+    'Height': [pokemon_data['height']],
+    'Weight': [pokemon_data['weight']],
+    'Base Experience': [pokemon_data['base_experience']],
+}
+
+df = pd.DataFrame(pokemon_info)
+engine = db.create_engine('sqlite:///data_base_name.db')
+df.to_sql('table_name', con=engine, if_exists='replace', index=False)
+
+with engine.connect() as connection:
+   query_result = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()
+   print(pd.DataFrame(query_result))
+
+
